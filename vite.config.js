be8984d6a9d09e57588c6f07ed6786b4cb7a7502
Aryan N. Jain <aryanj1084@gmail.com>
2024-10-10 20:25:05 +0530

    PortFolio

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
- 
-  plugins: [react()], 
-  base: "/3D-Portfolio", // Adjust the base URL to your repository name
+  plugins: [react()],
+  base: "/3D-Portfolio/", // YOUR REPO NAME HERE
 });
